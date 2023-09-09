f9cea8aedc77f441d794a304e52254e41ad3b36b
Williamcoy <williams.castiblancoc@uqvirtual.edu.co>
2023-09-08 21:27:19 -0500

    Se organiza la carpeta src

--- src/main/co/edu/uniquindio/clinica/modelo/Especializacion.java
+++
@@ -1,6 +0,0 @@
-package clinicaAvanzada.src.main.co.edu.uniquindio.clinica.modelo;
-
-public enum Especializacion {
-
-    NOMBRE;
-}
--- src/main/co/edu/uniquindio/clinica/modelo/Estado_PQRS.java
+++
@@ -1,4 +0,0 @@
-package clinicaAvanzada.src.main.co.edu.uniquindio.clinica.modelo;
-
-public enum Estado_PQRS {
-}
