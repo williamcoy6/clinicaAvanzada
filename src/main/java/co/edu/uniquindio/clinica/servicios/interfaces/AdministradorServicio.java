c3ce008efe66c9b2dbc89db03a50e74deb78c71e
Williamcoy <williams.castiblancoc@uqvirtual.edu.co>
2023-09-14 22:56:32 -0500

    se crean interfaces 1/5

---
+++ src/main/java/co/edu/uniquindio/clinica/servicios/interfaces/AdministradorServicio.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.clinica.servicios.interfaces;
+
+public interface AdministradorServicio {
+    int crearUsuario() throws Exception;
+
+    int actualizarUsuario() throws Exception;
+
+    int eliminarUsuario() throws Exception;
+
+}
---
+++ src/main/java/co/edu/uniquindio/clinica/servicios/interfaces/PqrsServicio.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.clinica.servicios.interfaces;
+
+public interface PqrsServicio {
+
+}
