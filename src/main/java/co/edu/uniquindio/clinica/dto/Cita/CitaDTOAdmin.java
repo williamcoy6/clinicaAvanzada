da9660a9bddd40b7e719fbaced33a107d7590d10
master2415 <duutabarquino2415@gmail.com>
2023-10-11 16:10:47 -0500

    Un par de métodos más para el Paciente

--- src/main/java/co/edu/uniquindio/clinica/dto/Cita/CitaDTOAdmin.java
+++ src/main/java/co/edu/uniquindio/clinica/dto/Cita/CitaDTOAdmin.java
@@ -2,15 +2,17 @@ package co.edu.uniquindio.clinica.dto.Cita;
 
 import co.edu.uniquindio.clinica.modelo.Enum.Especializacion;
 import co.edu.uniquindio.clinica.modelo.Enum.EstadoCita;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
-public record CitaDTOAdmin(String codigoCita,
-                           String cedulaPaciente,
-                           String nombrePaciente,
-                           String nombreMedico,
-                           LocalDateTime fecha,
-                           Especializacion especializacion,
-                           EstadoCita estadoCita ) {
+public record CitaDTOAdmin(int codigoCita,
+                           @NotBlank String cedulaPaciente,
+                           @NotBlank String nombrePaciente,
+                           @NotBlank String nombreMedico,
+                           @NotNull LocalDateTime fecha,
+                           @NotNull Especializacion especializacion,
+                           @NotNull EstadoCita estadoCita ) {
 
 }
